9bdf6116c2633db8438711811e0661a57b3276b4
Gaurang Agarwal <gaurangagarwal26@gmail.com>
2026-04-24 18:27:18 +0530

    file added

--- utils/Groq.js
+++ utils/Groq.js
@@ -2,20 +2,31 @@ import Groq from "groq-sdk";
 
 const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
-export async function main() {
-  const chatCompletion = await getGroqChatCompletion();
-  // Print the completion returned by the LLM.
-  console.log(chatCompletion.choices[0]?.message?.content || "");
-}
+// export async function main(prompt) {
+//   const chatCompletion = await getGroqChat(prompt);
+//   // Print the completion returned by the LLM.
+//   //console.log(chatCompletion.choices[0]?.message?.content || "");
+//   const content = chatCompletion.choices[0]?.message?.content;
+//   if (!content) {
+//     console.error("No response from model");
+//   } else {
+//     console.log(content);
+//   }
+//   //console.log(content);
+//   // const parsed = JSON.parse(content);
+//   // console.log(parsed);
+// }
 
-export async function getGroqChatCompletion(prompt) {
+export async function getGroqChat(prompt) {
   return groq.chat.completions.create({
     messages: [
       {
         role: "user",
-        content: {prompt},
+        content: prompt,
       },
     ],
     model: "openai/gpt-oss-20b",
   });
 }
+
+// main();
